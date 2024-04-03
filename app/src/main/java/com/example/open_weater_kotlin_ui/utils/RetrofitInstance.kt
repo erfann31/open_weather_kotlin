9d914e015bb35ddb0f7eb52b2c15809da2ff6cef
erfann31 <erfannasri2@gmail.com>
2024-04-03 07:51:31 +0330

    configuration Retrofit

---
+++ app/src/main/java/com/example/open_weater_kotlin_ui/utils/RetrofitInstance.kt
@@ -0,0 +1,39 @@
+package com.example.open_weater_kotlin_ui.utils
+
+import com.example.open_weater_kotlin_ui.domain.ApiInterface
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val APP_ID = "271d1234d3f497eed5b1d80a07b3fcd1"
+
+    val api: ApiInterface by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Util.Base)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+
+        retrofit.create(ApiInterface::class.java)
+
+        val loggingInterceptor = Interceptor { chain ->
+            val request = chain.request()
+//            Log.i("APIRequest", request.url.toString())
+            chain.proceed(request)
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        retrofit.newBuilder().client(client).build().create(ApiInterface::class.java)
+    }
+
+
+    fun getAppId(): String {
+        return APP_ID
+    }
+}
---
+++ app/src/main/java/com/example/open_weater_kotlin_ui/utils/Util.kt
@@ -0,0 +1,5 @@
+package com.example.open_weater_kotlin_ui.utils
+
+object Util {
+    const val Base = "https://api.openweathermap.org/"
+}
