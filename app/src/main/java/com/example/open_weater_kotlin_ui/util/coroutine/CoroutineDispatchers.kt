65f10c9317ef4e5ad2c63365a2c65ccf6e8792e2
erfann31 <erfannasri2@gmail.com>
2024-04-03 07:49:34 +0330

    delete files

--- app/src/main/java/com/example/open_weater_kotlin_ui/util/coroutine/CoroutineDispatchers.kt
+++
@@ -1,8 +0,0 @@
-package com.example.open_weater_kotlin_uiutil.coroutine
-
-import kotlin.coroutines.CoroutineContext
-
-interface CoroutineDispatchers {
-    val io: CoroutineContext
-    val main: CoroutineContext
-}
--- app/src/main/java/com/example/open_weater_kotlin_ui/util/coroutine/CoroutineDispatchersImpl.kt
+++
@@ -1,8 +0,0 @@
-package com.example.open_weater_kotlin_uiutil.coroutine
-
-import kotlinx.coroutines.Dispatchers
-
-object CoroutineDispatchersImpl : CoroutineDispatchers {
-    override val io = Dispatchers.IO
-    override val main = Dispatchers.Main
-}
