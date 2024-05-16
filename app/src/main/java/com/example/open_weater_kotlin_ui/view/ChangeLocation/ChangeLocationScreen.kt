35cd658a4330371428c163c94ccef1f4e9e90945
erfann31 <erfannasri2@gmail.com>
2024-05-16 19:49:05 +0330

    add search feature to ChangeLocationScreen

--- app/src/main/java/com/example/open_weater_kotlin_ui/view/ChangeLocation/ChangeLocationScreen.kt
+++ app/src/main/java/com/example/open_weater_kotlin_ui/view/ChangeLocation/ChangeLocationScreen.kt
@@ -5,23 +5,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.open_weater_kotlin_ui.R
+import com.example.open_weater_kotlin_ui.view.ChangeLocation.widgets.CityWidget
+import com.example.open_weater_kotlin_ui.view.ChangeLocation.widgets.RoundedSearchTextField
+import com.example.open_weater_kotlin_ui.viewModel.LocationInfoListener
 import com.example.open_weater_kotlin_ui.viewModel.WeatherViewModel
 
 @Composable
@@ -29,6 +33,13 @@ fun ChangeLocationScreen(
     navHostController: NavHostController,
     viewModel: WeatherViewModel = viewModel(),
 ) {
+    var isLoading by remember { mutableStateOf(false) }
+    viewModel.listener = object : LocationInfoListener {
+        override fun onLocationInfoFetched() {
+            isLoading = false
+            navHostController.navigate("hourly_forecast")
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -45,7 +56,7 @@ fun ChangeLocationScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     )
     {
-        val locationName = rememberSaveable { mutableStateOf("") }
+        var locationName by remember { mutableStateOf("") }
 
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -53,19 +64,42 @@ fun ChangeLocationScreen(
             modifier = Modifier.padding( 10.dp)
         )
         {
-            TextField(
-                value = locationName.value,
-                onValueChange = { locationName.value = it },
-                modifier = Modifier.fillMaxWidth(),
-                placeholder = { Text("Enter location name") }
+            RoundedSearchTextField(
+                placeholder = "Search for a city...",
+                text = locationName,
+                onSearchClicked = {
+                    isLoading = true
+                    viewModel.getLocationCoordinates(locationName)
+                },
+                onTextChanged = { newText ->
+                    locationName = newText
+                },
             )
-
             Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = { viewModel.getLocationCoordinates(locationName.value) },
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            ) {
-                Text("Get Weather")
+            if (isLoading) {
+                LazyColumn(
+                    Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    item {
+                        CircularProgressIndicator(color = Color.White)
+                    }
+                }
+            } else {
+                LazyColumn(Modifier.fillMaxSize()) {
+                    item {
+                        CityWidget(navHostController)
+                        CityWidget(navHostController)
+                        CityWidget(navHostController)
+                        CityWidget(navHostController)
+
+                    }
+                    item {
+                        CityWidget(navHostController, isAdd = true)
+
+                    }
+                }
             }
 }
 }
