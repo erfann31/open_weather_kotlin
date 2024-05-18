6f12ca10e49a315fe477370ae7b981431ec04a61
erfann31 <erfannasri2@gmail.com>
2024-05-18 06:05:45 +0330

    handle input intent from google map

--- app/src/main/java/com/example/open_weater_kotlin_ui/MainActivity.kt
+++ app/src/main/java/com/example/open_weater_kotlin_ui/MainActivity.kt
@@ -6,12 +6,15 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -21,12 +24,19 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.open_weater_kotlin_ui.models.WeatherRepository
 import com.example.open_weater_kotlin_ui.models.utils.RetrofitInstance
 import com.example.open_weater_kotlin_ui.view.navigation.Navigator
 import com.example.open_weater_kotlin_ui.view.theme.MyApplicationTheme
 import com.example.open_weater_kotlin_ui.viewModel.WeatherViewModel
 import com.example.open_weater_kotlin_ui.viewModel.WeatherViewModelFactory
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : ComponentActivity() {
     private val requestPermissionLauncher = registerForActivityResult(
@@ -35,23 +45,28 @@ class MainActivity : ComponentActivity() {
         if (isGranted) {
             checkGPS()
         } else {
-            // todo Permission denied
-        }
+            Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val intent = intent
         setContent {
             WeatherApp {
                 val context = LocalContext.current
-                if (ContextCompat.checkSelfPermission(
-                        context,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) == PackageManager.PERMISSION_GRANTED
-                ) {
-                    checkGPS()
+                if (intent?.action == Intent.ACTION_SEND && intent.type == "text/plain") {
+                    handleShareIntent(intent)
                 } else {
-                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                    if (ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        checkGPS()
+                    } else {
+                        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                    }
                 }
             }
         }
@@ -72,6 +87,91 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        handleShareIntent(intent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun handleShareIntent(intent: Intent?) {
+        if (intent?.action == Intent.ACTION_SEND && intent.type == "text/plain") {
+            val sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
+            sharedText?.let {
+                if (it.contains("http")) {
+                    resolveShortLink(it)
+                }
+            }
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun resolveShortLink(shortLink: String) {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val url = URL(shortLink)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.instanceFollowRedirects = false
+                val originalUrl = connection.getHeaderField("Location")
+                extractLatLonFromUrl(originalUrl)
+            } catch (e: Exception) {
+                Log.e("ResolveShortLink", "Error resolving short link: ${e.message}")
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun extractLatLonFromUrl(originalUrl: String?) {
+        originalUrl?.let {
+            val locationName =
+                originalUrl.split("place").lastOrNull()!!.split(",").firstOrNull()!!.split(",").firstOrNull()!!.removePrefix("/")
+                    .replace("+", " ")
+
+            Log.i("originalUrl", originalUrl)
+            Log.i("locationName", locationName)
+
+            val apiInterface = RetrofitInstance.api
+            val weatherRepository = WeatherRepository(apiInterface)
+            val factory = WeatherViewModelFactory(weatherRepository)
+            val viewModel = ViewModelProvider(this, factory)[WeatherViewModel::class.java]
+
+            if (locationName.matches(Regex("^[a-zA-Z].*")) == true) {
+                viewModel.getLocationCoordinates(locationName)
+                setContent {
+                    MyApplicationTheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            Navigator(viewModel)
+                        }
+                    }
+                }
+            } else {
+                val lat = originalUrl.split("place").lastOrNull()?.split(",")?.firstOrNull()?.removePrefix("/")?.toDoubleOrNull()
+                val lon = originalUrl.split("place").lastOrNull()?.split(",")?.get(1)?.split("/")?.firstOrNull()?.toDoubleOrNull()
+
+                Log.i("lat", lat!!.toDouble().toString())
+                Log.i("lon", lon.toString())
+
+                viewModel.viewModelScope.launch {
+                    viewModel.setLatLon(lat, lon!!)
+                }
+
+                setContent {
+                    MyApplicationTheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            Navigator(viewModel)
+                        }
+                    }
+                }
+            }
+        }
+    }
     private fun requestLocationUpdates(viewModel: WeatherViewModel) {
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if (ActivityCompat.checkSelfPermission(
@@ -85,11 +185,13 @@ class MainActivity : ComponentActivity() {
             return
         }
         locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, object : android.location.LocationListener {
+            @RequiresApi(Build.VERSION_CODES.O)
             override fun onLocationChanged(location: Location) {
+
                 val latitude = location.latitude
                 val longitude = location.longitude
 
-                viewModel.updateWeatherData(latitude, longitude)
+                viewModel.setLatLon(latitude, longitude)
 
                 if (latitude != null && longitude != null) {
                     setContent {
@@ -118,7 +220,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WeatherApp(content: @Composable () -> Unit) {
-    MaterialTheme {
+    MyApplicationTheme {
         Surface {
             content()
         }
