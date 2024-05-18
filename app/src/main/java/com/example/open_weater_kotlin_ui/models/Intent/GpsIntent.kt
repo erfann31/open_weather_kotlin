d3c24fc3fe47447290e8188f619e57e1d3ebb900
erfann31 <erfannasri2@gmail.com>
2024-05-18 05:40:54 +0330

    Add Gps Intent

---
+++ app/src/main/java/com/example/open_weater_kotlin_ui/models/Intent/GpsIntent.kt
@@ -0,0 +1,17 @@
+package com.example.open_weater_kotlin_ui.models.Intent
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import com.example.open_weater_kotlin_ui.viewModel.WeatherViewModel
+
+fun goToMap(viewModel: WeatherViewModel, locationName: String, context: Context) {
+    val lat = viewModel.lat.value
+    val lon = viewModel.lon.value
+    if (lat != null && lon != null) {
+        val gmmIntentUri = Uri.parse("geo:0,0?q=$lat,$lon($locationName)")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        context.startActivity(mapIntent)
+    }
+}
