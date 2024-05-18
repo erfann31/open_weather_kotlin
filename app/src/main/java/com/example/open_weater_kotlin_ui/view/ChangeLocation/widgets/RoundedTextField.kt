95177054ad7a38cf79d172f7b3751a3efdae6a4c
erfann31 <erfannasri2@gmail.com>
2024-05-19 01:48:34 +0330

    fix RoundedTextField icon

--- app/src/main/java/com/example/open_weater_kotlin_ui/view/ChangeLocation/widgets/RoundedTextField.kt
+++ app/src/main/java/com/example/open_weater_kotlin_ui/view/ChangeLocation/widgets/RoundedTextField.kt
@@ -3,6 +3,8 @@ package com.example.open_weater_kotlin_ui.view.ChangeLocation.widgets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -11,38 +13,36 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import com.example.open_weater_kotlin_ui.R
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import com.example.open_weater_kotlin_ui.R
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun RoundedTextField(
-    placeholderColor: Color=Color(0xFF929292),
+    modifier: Modifier = Modifier,
+    placeholderColor: Color = Color(0xFF929292),
     cursurColor: Color,
     textColor: Color,
-    containerColor:Color,
+    containerColor: Color,
     borderColor: Color,
-    isRounded:Boolean,
-    modifier: Modifier = Modifier,
+    isRounded: Boolean,
     text: String,
     showIcon: Boolean,
     textSize: TextUnit,
@@ -50,8 +50,8 @@ fun RoundedTextField(
     onSearchClicked: () -> Unit = {},
     onTextChanged: (String) -> Unit,
 ) {
-    var shape= RoundedCornerShape(5)
-    if (isRounded){
+    var shape = RoundedCornerShape(5)
+    if (isRounded) {
         shape = RoundedCornerShape(percent = 50)
     }
     val focusRequester = remember { FocusRequester() }
@@ -105,8 +105,8 @@ fun RoundedTextField(
         colors = OutlinedTextFieldDefaults.colors(
             cursorColor = cursurColor,
             focusedContainerColor = containerColor,
-            unfocusedContainerColor =  containerColor,
-            disabledContainerColor =  containerColor,
+            unfocusedContainerColor = containerColor,
+            disabledContainerColor = containerColor,
             focusedBorderColor = borderColor,
             unfocusedBorderColor = borderColor,
         ),
@@ -116,7 +116,11 @@ fun RoundedTextField(
                     modifier = Modifier.padding(horizontal = 12.dp),
                     onClick = onSearchClicked
                 ) {
-                    Icon(Icons.Default.Search, contentDescription = "Search")
+                    Icon(
+                        Icons.Default.Search,
+                        tint = colorResource(R.color.customBlue),
+                        contentDescription = "Search",
+                    )
                 }
             }
         },
