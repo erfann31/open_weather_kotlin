e729cd6bde3737905182efa34e5581ee4cfaa721
erfann31 <erfannasri2@gmail.com>
2024-05-18 12:38:37 +0330

    fix CityWidget

--- app/src/main/java/com/example/open_weater_kotlin_ui/view/ChangeLocation/widgets/CityWidget.kt
+++ app/src/main/java/com/example/open_weater_kotlin_ui/view/ChangeLocation/widgets/CityWidget.kt
@@ -1,20 +1,26 @@
 package com.example.open_weater_kotlin_ui.view.ChangeLocation.widgets
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,77 +30,105 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
 import com.example.open_weater_kotlin_ui.R
 
 @Composable
 fun CityWidget(
-    navController: NavHostController,
     modifier: Modifier = Modifier,
-    onClick: () -> Unit = {
-    },
+    text: String = stringResource(id = R.string.add_city),
+    onClick: () -> Unit = {},
+    iconOnClick: () -> Unit = {},
+    onTextChanged: (String) -> Unit = {},
+    textFieldValue: String = "",
+    isEdit: Boolean = false,
     isAdd: Boolean = false,
 ) {
-    val text = if (isAdd) {
-        stringResource(id = R.string.add_city)
-    } else {
-        "city"
-    }
+
+    var showTextField by remember { mutableStateOf(false) }
+
     Surface(
         onClick = onClick,
-        modifier = modifier.width(
-            width = LocalConfiguration.current.screenWidthDp.dp,
-        ),
+        modifier = modifier
+            .width(width = LocalConfiguration.current.screenWidthDp.dp)
+            .height(50.dp)
+            .padding(horizontal = 12.dp),
         color = Color(0x00000000),
     ) {
         Column(
             verticalArrangement = Arrangement.SpaceBetween,
         ) {
             Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(
-                    modifier = Modifier.padding(horizontal = 30.dp, vertical = 4.dp),
-                    text = text,
-                    style = TextStyle(
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight(700),
-                        fontFamily = FontFamily(
-                            Font(
-                                R.font.poppins_semibold,
-                            ),
-                        ),
-                        color =
-                            Color(0xFFFFFFFF)
+                if (showTextField) {
+                    RoundedSearchTextField(
+                        text = textFieldValue,
+                        onTextChanged = { onTextChanged(it) },
+                        placeholder = "Type name of city here...",
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(50.dp).padding(end =  40.dp),
+                        textSize = 12.sp,
+                        showIcon = false
                     )
-                )
-                    Icon(
-                        imageVector =
-                        if (isAdd){
-                            Icons.Filled.Add
-                        }
-                                else{
-                            Icons.Filled.Close
-
+                } else {
+                    Text(
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        text = text,
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight(700),
+                            fontFamily = FontFamily(
+                                Font(
+                                    R.font.poppins_semibold,
+                                ),
+                            ),
+                            color = Color(0xFFFFFFFF)
+                        )
+                    )
+                }
+                if (isEdit || isAdd) {
+                    IconButton(
+                        onClick = {
+                            if (isAdd) {
+                                if (showTextField) {
+                                    iconOnClick()
+                                    showTextField = false
+                                } else {
+                                    showTextField = true
+                                }
+                            } else {
+                                iconOnClick()
+                            }
                         },
                         modifier = Modifier
-                            .padding(horizontal = 20.dp, vertical = 8.dp)
+                            .padding(horizontal = 10.dp, vertical = 8.dp)
                             .size(30.0.dp)
-                            .clickable {
+                    ) {
+                        Icon(
+                            imageVector =
+                            if (showTextField) {
+                                Icons.Default.Check
+                            } else {
+                                if (isEdit) {
+                                    Icons.Default.Close
+                                } else {
+                                    Icons.Default.Add
+                                }
                             },
-
-                        contentDescription = "bottom_bar_item_active",
-                        tint =
-                            Color(0xFFE3E3E3)
-
-                    )
+                            contentDescription = "bottom_bar_item_active",
+                            tint = Color(0xFFE3E3E3)
+                        )
+                    }
+                }
             }
-
         }
     }
 }
+
