6b68c063f29b82f2c94301d9e7e63e6fa8d44d13
MotahareVakili <mv2971@yahoo.com>
2024-05-21 03:12:49 +0330

    add javadoc to itent and repository

--- app/src/main/java/com/example/open_weater_kotlin_ui/model/Intent/GpsIntent.kt
+++ app/src/main/java/com/example/open_weater_kotlin_ui/model/Intent/GpsIntent.kt
@@ -5,6 +5,18 @@ import android.content.Intent
 import android.net.Uri
 import com.example.open_weater_kotlin_ui.view_model.WeatherViewModel
 
+/**
+ * Opens Google Maps to display a specific location.
+ *
+ * This method retrieves the latitude and longitude coordinates from the provided ViewModel,
+ * constructs a URI using these coordinates and the location name, creates an Intent with
+ * the ACTION_VIEW action and this URI, sets Google Maps as the default app to handle the Intent,
+ * and finally executes the Intent to open Google Maps.
+ *
+ * @param viewModel The ViewModel containing the latitude and longitude coordinates.
+ * @param locationName The name of the location to be displayed on Google Maps.
+ * @param context The application context used to start the Google Maps activity.
+ */
 fun goToMap(viewModel: WeatherViewModel, locationName: String, context: Context) {
     val lat = viewModel.lat.value
     val lon = viewModel.lon.value
--- app/src/main/java/com/example/open_weater_kotlin_ui/model/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/open_weater_kotlin_ui/model/repository/WeatherRepositoryImpl.kt
@@ -9,25 +9,71 @@ import com.example.open_weater_kotlin_ui.model.entities.LocationCoordinate
 import com.example.open_weater_kotlin_ui.model.utils.RetrofitInstance
 import retrofit2.Response
 
+/**
+ * Implementation of the WeatherRepository interface to fetch weather data from the API.
+ *
+ * @property apiInterface The interface for API communication.
+ *
+ * @author Erfan Nasri
+ */
 class WeatherRepositoryImpl(private val apiInterface: ApiInterface) : WeatherRepository {
     private val appId = RetrofitInstance.getAppId()
+
+    /**
+     * Retrieves location coordinates by the given location name from the API.
+     *
+     * @param locationName The name of the location.
+     * @return A Response object containing a list of LocationCoordinate objects.
+     */
     override suspend fun getCoordinatesByLocationName(locationName: String): Response<List<LocationCoordinate>> {
         return apiInterface.getCoordinatesByLocationNameAsync(locationName, appId).await()
     }
 
+
+    /**
+     * Performs reverse geocoding to retrieve location coordinates by latitude and longitude.
+     *
+     * @param lat The latitude coordinate.
+     * @param lon The longitude coordinate.
+     * @return A Response object containing a list of LocationCoordinate objects.
+     */
     override suspend fun reverseGeocoding(lat: Double, lon: Double): Response<List<LocationCoordinate>> {
         return apiInterface.reverseGeocodingAsync(lat, lon, appId).await()
     }
 
+    /**
+     * Retrieves the current weather data based on the given coordinates.
+     *
+     * @param lat The latitude coordinate.
+     * @param lon The longitude coordinate.
+     * @param unit The unit of measurement for temperature.
+     * @return A Response object containing the CurrentWeather data.
+     */
     override suspend fun getCurrentWeatherData(lat: Double, lon: Double, unit: String): Response<CurrentWeather> {
         return apiInterface.getCurrentWeatherDataAsync(lat, lon, unit, appId).await()
     }
 
 
+    /**
+     * Retrieves the daily forecast based on the given coordinates.
+     *
+     * @param lat The latitude coordinate.
+     * @param lon The longitude coordinate.
+     * @param unit The unit of measurement for temperature.
+     * @return A Response object containing the ForecastDaily data.
+     */
     override suspend fun getDailyForecast(lat: Double, lon: Double, unit: String): Response<ForecastDaily> {
         return apiInterface.getDailyForecastAsync(lat, lon, unit, appId).await()
     }
 
+    /**
+     * Retrieves the hourly forecast based on the given coordinates.
+     *
+     * @param lat The latitude coordinate.
+     * @param lon The longitude coordinate.
+     * @param unit The unit of measurement for temperature.
+     * @return A Response object containing the ForecastHourly data.
+     */
     override suspend fun getHourlyForecast(lat: Double, lon: Double, unit: String): Response<ForecastHourly> {
         return apiInterface.getHourlyForecastAsync(lat, lon, unit, appId).await()
     }
