a9a99c2e903ec30c49b8eb1a114fc97874384140
erfann31 <erfannasri2@gmail.com>
2024-05-22 04:35:03 +0330

    fix background color

--- app/src/main/java/com/example/open_weater_kotlin_ui/view/theme/Theme.kt
+++ app/src/main/java/com/example/open_weater_kotlin_ui/view/theme/Theme.kt
@@ -3,12 +3,9 @@ package com.example.open_weater_kotlin_ui.view.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -20,20 +17,10 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.colorResource
-import androidx.core.view.WindowCompat
 import com.example.open_weater_kotlin_ui.R
 import com.example.open_weater_kotlin_uiui.theme.Pink40
-import com.example.open_weater_kotlin_uiui.theme.Pink80
 import com.example.open_weater_kotlin_uiui.theme.Purple40
-import com.example.open_weater_kotlin_uiui.theme.Purple80
 import com.example.open_weater_kotlin_uiui.theme.PurpleGrey40
-import com.example.open_weater_kotlin_uiui.theme.PurpleGrey80
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
@@ -51,20 +38,17 @@ private val LightColorScheme = lightColorScheme(
 
     )
 
-
 @Composable
 fun MyApplicationTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            dynamicLightColorScheme(context).copy(background = colorResource(R.color.teal_700))
         }
 
-        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
@@ -73,7 +57,6 @@ fun MyApplicationTheme(
             val window = (view.context as Activity).window
             window.statusBarColor = Color(0xFF4DFFF6).toArgb()
             window.navigationBarColor = Color(0xFF004451).toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
