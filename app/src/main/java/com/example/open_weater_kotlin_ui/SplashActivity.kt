31004219f30eaf9af0e3203d17e8d230cd7a9384
erfann31 <erfannasri2@gmail.com>
2024-05-24 23:53:41 +0330

    fix SplashScreen

--- app/src/main/java/com/example/open_weater_kotlin_ui/SplashActivity.kt
+++ app/src/main/java/com/example/open_weater_kotlin_ui/SplashActivity.kt
@@ -1,22 +1,16 @@
 package com.example.open_weater_kotlin_ui
 
-import android.Manifest
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.LocationManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.provider.Settings
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,9 +19,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.vectorResource
-import androidx.core.content.ContextCompat
-import com.example.open_weater_kotlin_ui.MainActivity
-import com.example.open_weater_kotlin_ui.R
+import com.example.open_weater_kotlin_ui.view.theme.MyApplicationTheme
 
 /**
  * SplashActivity displays a splash screen while checking for location permissions
@@ -35,45 +27,13 @@ import com.example.open_weater_kotlin_ui.R
  *
  * @author Erfan Nasri
  */
- class SplashActivity : ComponentActivity() {
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            checkGPS()
-        } else {
-            Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
-            navigateToMainActivity()
-        }
-    }
-
+class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SplashScreenContent()
-        }
-    }
-
-    @Composable
-    fun SplashScreenContent() {
-        Box(
-            modifier = Modifier.fillMaxSize().background(
-                Brush.verticalGradient(
-                    listOf(
-                        colorResource(R.color.customCyan),
-                        colorResource(R.color.customBlue)
-                    )
-                )
-            ),
-            contentAlignment = Alignment.Center,
-        ) {
-            // Customize your splash screen UI here
-            Icon(
-                imageVector = ImageVector.vectorResource(id = R.drawable.app_icon),
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
-                tint = Color.White
-            )
+            SplashScreen {
+                navigateToMainActivity()
+            }
         }
     }
 
@@ -83,46 +43,37 @@ import com.example.open_weater_kotlin_ui.R
             finish()
         }, 3000)
     }
+}
 
-     /**
-      * Checks if GPS is enabled, and if not, prompts the user to enable it.
-      */
-    private fun checkGPS() {
-        if (isGPSEnabled()) {
-            navigateToMainActivity()
-        } else {
-            Toast.makeText(this, "Please enable GPS", Toast.LENGTH_LONG).show()
-            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
-        }
-    }
-
-     /**
-      * Checks if GPS is enabled.
-      *
-      * @return true if GPS is enabled, false otherwise.
-      */
-    private fun isGPSEnabled(): Boolean {
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        checkLocationPermission()
-    }
-
-     /**
-      * Checks location permission and proceeds accordingly.
-      */
-    private fun checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
+@Composable
+fun SplashScreen(onTimeout: () -> Unit) {
+    MyApplicationTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
         ) {
-            checkGPS()
-        } else {
-            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            listOf(
+                                colorResource(R.color.customCyan),
+                                colorResource(R.color.customBlue)
+                            )
+                        )
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                val imageVector = ImageVector.vectorResource(id = R.drawable.app_icon)
+                Icon(
+                    imageVector = imageVector,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    tint = Color.White // Set the color to white
+                )
+            }
         }
     }
+
+    Handler(Looper.getMainLooper()).postDelayed(onTimeout, 3000)
 }
